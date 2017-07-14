4f7d75a26f1b6fcf56470f2b15998c936778d7ea
Lucas Reich <L-Reich@web.de>
2017-07-14 14:09:47 +0200

    .

--- app/src/main/java/com/example/lucas/haushaltsmanager/ExpenseScreen.java
+++ app/src/main/java/com/example/lucas/haushaltsmanager/ExpenseScreen.java
@@ -1,65 +1,153 @@
 package com.example.lucas.haushaltsmanager;
 
-import android.content.Context;
-import android.content.DialogInterface;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.text.InputType;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class ExpenseScreen extends AppCompatActivity {
 
-    private String m_Text = "";
-
     @Override
-    protected  void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.expense_screen);
 
-        Intent intent = getIntent();
-        String value = intent.getStringExtra("key");
+        TextView amountPopUp = (TextView) findViewById(R.id.expense_screen_amount);
+        amountPopUp.setOnFocusChangeListener(new View.OnFocusChangeListener() {
 
-        Context context = getApplicationContext();
-        String rückgabe = value;
-        int duration = Toast.LENGTH_SHORT;
-
-        Toast toast = Toast.makeText(context, rückgabe, duration);
-        toast.show();
-
-        TextView textView = (TextView) findViewById(R.id.editText);
-    }
-
-    public void popTextInput(View view) {
-
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle("Title");
-
-// Set up the input
-        final EditText input = new EditText(this);
-// Specify the type of input expected; this, for example, sets the input as a password, and will mask the text
-        input.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
-        builder.setView(input);
-
-// Set up the buttons
-        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
             @Override
-            public void onClick(DialogInterface dialog, int which) {
-                m_Text = input.getText().toString();
-            }
-        });
-        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                dialog.cancel();
+            public void onFocusChange(View view, boolean hasFocus) {
+
+                if (hasFocus) {
+
+                    Bundle bundle = new Bundle();
+                    String dialogTitle = "";
+                    bundle.putString("title", dialogTitle);
+
+                    ExpenseInputDialogFragment accountPopUp = new ExpenseInputDialogFragment();
+                    accountPopUp.setArguments(bundle);
+                    accountPopUp.show(getFragmentManager(), "feuern");
+                }
             }
         });
 
-        builder.show();
+        EditText categoryPopUp = (EditText) findViewById(R.id.expense_screen_category);
+        categoryPopUp.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+
+            @Override
+            public void onFocusChange(View view, boolean hasFocus) {
+
+                if (hasFocus) {
+
+                    Bundle bundle = new Bundle();
+                    String dialogTitle = "Kategorie";
+                    bundle.putString("title", dialogTitle);
+
+                    ExpenseInputDialogFragment accountPopUp = new ExpenseInputDialogFragment();
+                    accountPopUp.setArguments(bundle);
+                    accountPopUp.show(getFragmentManager(), "feuern");
+                }
+            }
+        });
+
+        EditText tagPopUp = (EditText) findViewById(R.id.expense_screen_tag);
+        tagPopUp.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+
+            @Override
+            public void onFocusChange(View view, boolean hasFocus) {
+
+                if (hasFocus) {
+
+                    Bundle bundle = new Bundle();
+                    String dialogTitle = "Merkmal";
+                    bundle.putString("title", dialogTitle);
+
+                    ExpenseInputDialogFragment accountPopUp = new ExpenseInputDialogFragment();
+                    accountPopUp.setArguments(bundle);
+                    accountPopUp.show(getFragmentManager(), "feuern");
+                }
+            }
+        });
+
+        EditText titlePopUp = (EditText) findViewById(R.id.expense_screen_title);
+        titlePopUp.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+
+            @Override
+            public void onFocusChange(View view, boolean hasFocus) {
+
+                if (hasFocus) {
+
+                    Bundle bundle = new Bundle();
+                    String dialogTitle = "Titel";
+                    bundle.putString("title", dialogTitle);
+
+                    ExpenseInputDialogFragment accountPopUp = new ExpenseInputDialogFragment();
+                    accountPopUp.setArguments(bundle);
+                    accountPopUp.show(getFragmentManager(), "feuern");
+                }
+            }
+        });
+
+        EditText datePopUp = (EditText) findViewById(R.id.expense_screen_date);
+        datePopUp.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+
+            @Override
+            public void onFocusChange(View view, boolean hasFocus) {
+
+                if (hasFocus) {
+
+                    Bundle bundle = new Bundle();
+                    String dialogTitle = "Datum";
+                    bundle.putString("title", dialogTitle);
+
+                    ExpenseInputDialogFragment accountPopUp = new ExpenseInputDialogFragment();
+                    accountPopUp.setArguments(bundle);
+                    accountPopUp.show(getFragmentManager(), "feuern");
+                }
+            }
+        });
+
+        EditText noticePopUp = (EditText) findViewById(R.id.expense_screen_notice);
+        noticePopUp.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+
+            @Override
+            public void onFocusChange(View view, boolean hasFocus) {
+
+                if (hasFocus) {
+
+                    Bundle bundle = new Bundle();
+                    String dialogTitle = "Notiz";
+                    bundle.putString("title", dialogTitle);
+
+                    ExpenseInputDialogFragment accountPopUp = new ExpenseInputDialogFragment();
+                    accountPopUp.setArguments(bundle);
+                    accountPopUp.show(getFragmentManager(), "feuern");
+                }
+            }
+        });
+
+        EditText accountPopUp = (EditText) findViewById(R.id.expense_screen_account);
+        accountPopUp.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+
+            @Override
+            public void onFocusChange(View view, boolean hasFocus) {
+
+                if (hasFocus) {
+
+                    Bundle bundle = new Bundle();
+                    String dialogTitle = "Konten";
+                    bundle.putString("title", dialogTitle);
+
+                    ExpenseInputDialogFragment accountPopUp = new ExpenseInputDialogFragment();
+                    accountPopUp.setArguments(bundle);
+                    accountPopUp.show(getFragmentManager(), "feuern");
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/example/lucas/haushaltsmanager/MainActivity.java
+++ app/src/main/java/com/example/lucas/haushaltsmanager/MainActivity.java
@@ -1,9 +1,9 @@
 package com.example.lucas.haushaltsmanager;
 
 import android.content.Intent;
+import android.graphics.*;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -13,14 +13,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -34,6 +36,7 @@ public class MainActivity extends AppCompatActivity
                 MainActivity.this.startActivity(intent);
             }
         });
+
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -81,23 +84,8 @@ public class MainActivity extends AppCompatActivity
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
-        /*int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
-        }*/
-
-        switch(item.getItemId()){
+        switch (item.getItemId()) {
 
             case R.id.desktop:
                 //do smth
