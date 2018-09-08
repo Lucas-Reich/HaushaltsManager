0a05a47ae4e1aec79a8c7e284e1fbe4786b511aa
Lucas Reich <L-Reich@web.de>
2018-09-08 10:10:20 +0200

    unnötigen header entfernt

--- app/src/main/java/com/example/lucas/haushaltsmanager/Dialogs/ChangelogDialog/ChangelogItem.java
+++ app/src/main/java/com/example/lucas/haushaltsmanager/Dialogs/ChangelogDialog/ChangelogItem.java
@@ -2,5 +2,7 @@ package com.example.lucas.haushaltsmanager.Dialogs.ChangelogDialog;
 
 public interface ChangelogItem {
     String getDescription();
+
     String getType();
+    //get color? um die einträge in der liste farbig darzustellen
 }
